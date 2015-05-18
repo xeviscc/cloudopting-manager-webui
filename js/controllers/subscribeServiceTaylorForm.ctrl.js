0bd5b82d52fd28781b03f08651af28920426e295
Xavier Cases <xavier.cases.external@atos.net>
2015-05-18 16:03:22 +0200

    Few updates on views

--- js/controllers/subscribeServiceTaylorForm.ctrl.js
+++ js/controllers/subscribeServiceTaylorForm.ctrl.js
@@ -1,12 +1,29 @@
 define(
     [
-        'app'
+        'app',
+        'services/rest-api-wrap',
+        'services/application.service'
     ],
     function(app) {
         app.controller('SubscribeServiceTaylorFormCtrl',
-            [ '$scope',
-                function ($scope) {
+            ['$scope', '$log', 'RestApi', 'ApplicationService',
+                function ($scope, $log, RestApi, ApplicationService) {
                     'use strict';
+
+                    if(ApplicationService.currentApplication !== undefined
+                        && ApplicationService.currentApplication !== null)
+                    {
+                        var screen = RestApi.inputParameters(ApplicationService.currentApplication.id);
+                        $scope.cloudNodeList = screen.cloudNodeList;
+                        $scope.osList = screen.osList;
+                        $scope.skinList = screen.skinList;
+                    }
+                    else
+                    {
+                        //Show some error or tell user to go and select a Service.
+                        //throw Exception!!
+                        //throw new WebUIException("Application not present.");
+                    }
 
                     $scope.requestSubscription = function(){
                         //Request Subscription¿?
