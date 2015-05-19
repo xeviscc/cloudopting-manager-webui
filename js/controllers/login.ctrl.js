3fad8a59f255b15e3fa1b2bc04872b85f15fd880
Xavier Cases <xavier.cases.external@atos.net>
2015-05-19 12:49:33 +0200

    Upgraded menu

--- js/controllers/login.ctrl.js
+++ js/controllers/login.ctrl.js
@@ -4,14 +4,14 @@ define(
     ],
     function(app) {
         app.controller('LoginCtrl',
-            ['$cookies', '$translate', '$scope', '$state', 'AuthenticationService',
-                function ($cookies, $translate, $scope, $state, AuthenticationService) {
+            ['$cookies', '$translate', '$scope', '$location', 'AuthenticationService', 'RoleService',
+                function ($cookies, $translate, $scope, $location, AuthenticationService, RoleService) {
                     'use strict';
 
                     var userWithRole = AuthenticationService.loginByCookie();
                     if(userWithRole) {
                         //Change page
-                        $state.go('serviceCatalogList');
+                        changePage(userWithRole);
                     }
                     //else ask for credentials
 
@@ -22,10 +22,34 @@ define(
 
                         if(userWithRole) {
                             //Change page
-                            $state.go('serviceCatalogList');
+                            changePage(userWithRole);
                         } else {
                             //If does not exists, show error message.
                             $scope.error = $translate.instant('login.error');
+                        }
+                    };
+
+                    /**
+                     * Private function to change location depending on the role.
+                     * @param user
+                     */
+                    function changePage(user) {
+                        if(RoleService.isRoleAdmin(user)
+                            || RoleService.isRoleSubscriber(user))
+                        {
+                            $location.path('/serviceCatalogList');
+                        }
+                        else if(RoleService.isRoleOperator(user))
+                        {
+                            $location.path('/instances');
+                        }
+                        else if(RoleService.isRolePublisher(user))
+                        {
+                            $location.path('/servicesCatalog');
+                        }
+                        else
+                        {
+                            $location.path('/serviceCatalogList');
                         }
                     }
                 }
