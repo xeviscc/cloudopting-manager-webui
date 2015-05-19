89280838d875d996b95f994c0d484e9652a46579
Xavier Cases <xavier.cases.external@atos.net>
2015-05-19 09:29:20 +0200

    Added some screen customization depending on role.

--- js/controllers/detailService.ctrl.js
+++ js/controllers/detailService.ctrl.js
@@ -4,15 +4,54 @@ define(
         'services/application.service'
     ],
     function(app) {
-        app.controller('DetailServiceCtrl',['$scope', '$log', '$location', 'RestApi', 'ApplicationService',
-                function($scope, $log, $location, RestApi, ApplicationService) {
+        app.controller('DetailServiceCtrl',['$translate', '$scope', '$log', '$location', 'RestApi', 'ApplicationService', 'SessionService', 'RoleService',
+                function($translate, $scope, $log, $location, RestApi, ApplicationService, SessionService, RoleService) {
                     'use strict';
 
                     $scope.appDetail = ApplicationService.currentApplication;
+                    $scope.showButton = true;
 
-                    $scope.subscribe = function() {
-                        $location.path('/subscribeServiceTaylorForm');
+                    //IF the status of the services is "UNFINISHED" we have to set the button "GO TO EDIT" if it is the Publisher
+
+                    if(RoleService.isRolePublisher(SessionService.role)){
+                        //TODO: Define all status possible for an APPLICATION.
+                        if(appDetail.status === "UNFINISHED"){
+                            $scope.detail_function = function() {
+                                $location.path('/publish');
+                                //TODO: Set the current application to the application service?¿?
+                                //ApplicationService.currentApplication = appDetail;
+                            };
+                            $scope.buttonValue = $translate.instant('detail.button.completepublish');
+                        } else {
+                            $scope.detail_function = function() {
+                                $location.path('/instances');
+                            };
+                            $scope.buttonValue = $translate.instant('detail.button.instances');
+                        }
                     }
+                    if(RoleService.isRoleAdmin(SessionService.role)
+                        || RoleService.isRoleOperator(SessionService.role))
+                    {
+                        $scope.detail_function = function() {
+                            $location.path('/instances');
+                        };
+                        $scope.buttonValue = $translate.instant('detail.button.instances');
+                    }
+                    else if(RoleService.isRoleSubscriber(SessionService.role))
+                    {
+                        $scope.detail_function = function() {
+                            $location.path('/subscribeServiceTaylorForm');
+                        };
+                        $scope.buttonValue = $translate.instant('detail.button.subscribe');
+                    }
+                    else
+                    {
+                        //hide button.
+                        //If no detail_funtion is defined, the button will remain hidden.
+                        $scope.showButton = false;
+                    }
+
+
                 }
             ]
         )
--- js/controllers/instancesServiceCatalog.ctrl.js
+++ js/controllers/instancesServiceCatalog.ctrl.js
@@ -10,12 +10,31 @@ define(
                 function ($scope, $log, $location, RestApi, ApplicationService) {
                     'use strict';
 
+
+
                     //TODO: Change instancesList to applicationList once it is developed properly?¿?
                     $scope.instancesList = RestApi.instancesList();
 
                     //TODO: Implement button "Search Service" functionality.
 
                     //TODO: Implement button go for each instance.
+
+
+                    $scope.showDeploy = function(str){
+                        return str === "Requested";
+                    };
+                    $scope.showStop = function(str){
+                        return str === "Running";
+                    };
+                    $scope.showDelete = function(str){
+                        return str === "Running";
+                    };
+                    $scope.showStart = function(str){
+                        return str === "Stopped";
+                    };
+                    $scope.showMonitor = function(str){
+                        return str === "Running";
+                    };
                 }
             ]
         )
